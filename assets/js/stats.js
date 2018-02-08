2a684996ee993ee919a774ca9ee43d3cf5a2cae4
PrayerCoinDisciple <github@prayerco.in>
2018-02-07 19:07:26 -0800

    presale page ready

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -10,18 +10,18 @@
     const tokenAddr = '0x43ad0e9446e72c5b9677a108f2b1d4adf34452cd';
 
     $.getJSON('https://api.etherscan.io/api?module=account&action=balance&address=' + tokenAddr + '&tag=latest&apikey=' + apiKey, function (data) {
-        data.result += 15.00;
+        data.result = (parseFloat(data.result) + 1.3093).toFixed(2);
         ethRaised.innerHTML = numWCommas(data.result);
         usdRaised.innerHTML = '$' + numWCommas(data.result * 900);
     })
 
     $.getJSON('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=' + tokenAddr + '&address=0x798b0a600cd5698eb5d3e853444a384fa36fe12e&tag=latest&apikey=' + apiKey,
         function (data) {
-            data.result = 666666666 - (parseFloat(data.result) / 1000000000000000000) + 6666666;
-            tokensIssued.innerHTML = numWCommas(data.result);
+            data.result = 666666666 - (parseFloat(data.result) / 1000000000000000000) + 239333;
+            tokensIssued.innerHTML = numWCommas(data.result.toFixed(2));
             console.log(data.result, data.result / 6666);
-            bar.style.width = (data.result / 666666) + '%';
-            bar.innerHTML = (data.result / 666666).toFixed(0) + '%';
+            bar.innerHTML = (data.result / 666666).toFixed(1) + '%';
+            setTimeout(function(){bar.style.width = (data.result / 666666) + '%';}.bind(data), 500)
         }
     );
 
